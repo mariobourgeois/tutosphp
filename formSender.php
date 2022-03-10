3d7911643a6790c66d4f1ae02ebe236f56e9076a
mariobourgeois <mariobourgeois97.200@gmail.com>
2022-03-10 16:18:41 -0400

    form

---
+++ formSender.php
@@ -0,0 +1,18 @@
+<form input action="formReceiver.php" method="post">
+
+    <label form=""> Nom : </label>
+    <input type="text" name="nom" >
+    <input type="submit" name="Envoyer">
+
+</form>
+
+
+<form input action="formReceiver.php" method="get">
+    
+    <label form=""> Nom : </label>
+    <input type="text" name="nom" >
+    <label form=""> Prenom: </label>
+    <input type="text" name="prenom" >
+    <input type="submit" name="Envoyer">
+
+</form>
